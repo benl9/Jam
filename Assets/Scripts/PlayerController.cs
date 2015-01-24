6d58bd36fcd10b555253f004f1a1a11d494bf8d4
Ben Lin <ben.lin9@gmail.com>
2015-01-24 13:15:05 -0600

    added flipping player sprite

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,7 +14,9 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// calculuate x velocity
 		float x = Input.GetAxis ("Horizontal") * maxVelocity;
+		//calculate y velocity
 		float y; 
 		if (jump && Input.GetButtonDown ("Jump")) {
 			y = maxJump;
@@ -22,8 +24,17 @@ public class PlayerController : MonoBehaviour {
 		else {
 			y = rigidbody2D.velocity.y;
 		}
+		// set velocity
 		Vector2 velocityP = new Vector2(x,y);
-		rigidbody2D.velocity = velocityP; 
+		rigidbody2D.velocity = velocityP;
+		
+		//make sure sprite is facing the right way
+		Vector3 scale = transform.localScale;
+		if(x > 0 && scale.x != 1)
+			scale.x = 1;
+		else if(x < 0 && scale.x != -1)
+			scale.x = -1;
+		transform.localScale = scale;
 	}
 	void OnCollisionEnter2D(Collision2D collision){
 		if (collision.gameObject.tag == "Ground") {
