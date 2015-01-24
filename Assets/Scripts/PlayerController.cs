3cde3b87175dbc707cdcd1e4d1139fb6afdbaed9
sbear260 <sbear260molten@gmail.com>
2015-01-23 23:27:57 -0600

    fixed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,14 +2,26 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+	public Vector2 spawn;
+	public float maxVelocity;
+	public float maxJump; 
+	public GameObject player; 
 	// Use this for initialization
 	void Start () {
-		DontDestroyOnLoad(this);
+		DontDestroyOnLoad(player);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		float x = Input.GetAxis ("Horizontal") * maxVelocity;
+		float y = Input.GetButtonDown ("Jump") ? maxJump : rigidbody2D.velocity.y;
+		Vector2 velocityP = new Vector2(x,y);
+		rigidbody2D.velocity = velocityP; 
+	}
+	void OnCollisionEnter2D(Collision2D collision){
+		maxJump = 10; 
+	}
+	void OnCollisionExit2D(Collision2D collision){
+		maxJump = 0; 
 	}
 }
