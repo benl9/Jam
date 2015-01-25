2aec0ff9b08f686aba4fcd6ff1c3c321203264f3
insanity-chu <nicole.muzquiz@gmail.com>
2015-01-24 20:18:44 -0600

    Level

---
+++ Assets/Scripts/KamikazeAI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KamikazeAI : MonoBehaviour {
+
+	Transform player;
+	public Transform target; 
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		//enemy = GameObject.FindGameObjectWithTag ("Enemy").transform;
+
+		//enemy = GameObject.FindGameObjectWithTag ("Enemy").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		float enemyX = target.transform.position.x;
+		float playerX = player.position.x; 
+		if(Mathf.Abs (enemyX - playerX) <= 3) {
+			target.rigidbody2D.gravityScale = 4;
+
+		}
+	}
+
+}
---
+++ Assets/Scripts/RotatingDeath.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingDeath : MonoBehaviour {
+
+	public float speed = 15;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rigidbody2D.MoveRotation (rigidbody2D.rotation + speed * Time.deltaTime);
+	}
+}
